28d60e284c6bd6b130b9e64b6adb310142f556af
ka2jun8 <ka210n8@gmail.com>
2020-08-01 01:08:38 +0900

    client

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment }  from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
+import { LaunchTile, Header, Button, Loading } from "../components"
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -18,10 +19,48 @@ export const LAUNCH_TILE_DATA = gql`
   }
 `;
 
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
+    }
+  }
+`
+
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
+  const { data, loading, error, fetchMore } = useQuery(GET_LAUNCHES);
+  if (loading) return <Loading />
+  if (error) return <p>ERROR</p>
+
+  console.log({data})
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any )=> (
+          <LaunchTile
+            key={launch.id}
+            launch={launch}
+          />
+        ))}
+    </Fragment>
+  );
 }
 
 export default Launches;
